95effcf327f8d791330c536eb6607dfeddf9d296
sotogito <sotogitoarchive@gmail.com>
2025-03-24 11:35:30 +0900

    Initial commit

---
+++ sooyeon-jdbc-workspace/06_mvc/src/main/java/com/sotogito/run/Application.java
@@ -0,0 +1,34 @@
+package com.sotogito.run;
+
+public class Application {
+    public static void main(String[] args) {
+        /**
+         * ## MVC 패턴
+         * 1. 자바 백엔드에서 가장 대중적으로 사용되는 패턴
+         * 2. 데이터(Model), UI담당(View), 요청처리(Controller)을 역할별로 명확히 분리해서 작성
+         * 3. 장점
+         *      - 개발 속도 향상
+         *      - 재사용성 증가
+         *      - 유지보수성 증가
+         *      - 확장성
+         *
+         *
+         *
+         * ### VIEW
+         * - 입력
+         * - 출력
+         *
+         * ### CONTROLLER
+         * - 전달된 값 검증 및 가공
+         * - DTO에 담기
+         *
+         * ### SERIVCE
+         * - 비지니스 작업 순차적으로 수행
+         *
+         * ### DAO
+         * - DB에 접근
+         * - JDBC과정으로 SQL문 실행 및 결과 받기
+         */
+    }
+
+}
