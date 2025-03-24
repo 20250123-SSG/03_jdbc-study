bca9ff3c768404598afdc23b94bd936fab0d3b58
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-03-24 14:55:13 +0900

    create 05_service-layer

---
+++ seoyeon-jdbc-workspace/05_service-layer/src/main/java/com/podoseee/model/dao/MenuDAO.java
@@ -0,0 +1,101 @@
+package com.podoseee.model.dao;
+
+import com.podoseee.model.dto.CategoryDTO;
+import com.podoseee.model.dto.MenuDTO;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.podoseee.common.JDBCTemplate.close;
+
+public class MenuDAO {
+
+    private Properties prop = new Properties();
+
+    public MenuDAO(){
+        try {
+            prop.loadFromXML(new FileInputStream("src/main/java/com/podoseee/mapper/menu-query.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 신규 메뉴 추가용
+    public int insertMenu(Connection conn, MenuDTO menu){
+        PreparedStatement pstmt = null;
+        int result = 0; // 처리 결과를 받을 변수
+
+        String query = prop.getProperty("insertMenu");
+
+        try {
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, menu.getMenuName());
+            pstmt.setInt(2, menu.getMenuPrice());
+            pstmt.setInt(3, menu.getCategoryCode());
+            pstmt.setString(4, menu.getOrderableStatus());
+
+            result = pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally{
+            close(pstmt);
+        }
+
+        return result;
+
+    }
+
+    // 신규 카테고리 등록용
+    public int insertCategory(Connection conn, CategoryDTO category){
+        PreparedStatement pstmt = null;
+        int result = 0;
+
+        String query = prop.getProperty("insertCategory");
+
+        try {
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, category.getCategoryName());
+            pstmt.setObject(2, category.getRefCategoryCode());
+
+            result = pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally{
+            close(pstmt);
+        }
+
+        return result;
+    }
+
+
+    public int selectCurrentCategoryCode(Connection conn) {
+        // select문 => 조회결과 한행한열(하나의 숫자값) => ResultSet => int
+        int currentCategoryCode = 0; // 최종결과를 담을 변수
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+        String query = prop.getProperty("selectCurrentCategoryCode");
+
+        try {
+            pstmt = conn.prepareStatement(query);
+            rset = pstmt.executeQuery(); // rset에 담아줘야함
+
+            if(rset.next()){
+                currentCategoryCode = rset.getInt("curr_category_code");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(rset);
+            close(pstmt);
+        }
+
+        return currentCategoryCode;
+
+    }
+}
---
+++ seoyeon-jdbc-workspace/05_service-layer/src/main/java/com/podoseee/model/service/MenuService.java
@@ -0,0 +1,79 @@
+package com.podoseee.model.service;
+
+/*
+    ## Service ##
+    1. 비즈니스 로직 처리와 트랜잭션 관리를 담당
+       - 사용자의 요청에 따라 순차적으로 실행해야되는 작업들을 하나로 묶어 관리
+       - 중간과정에 문제 발생시 rollback 진행해야되므로 하나의 트랜잭션으로 묶어 관리
+       - 사용자인터페이스(UI)와 데이터베이스(Model) 사이의 계층
+    2. 처리 과정
+       1) Connection 생성
+       2) 순차적으로 작업 실행
+       3) 트랜잭션 처리가 필요할 경우 트랜잭션 처리
+       4) Connection 반납
+
+    * 비즈니스 로직 : 데이터베이스와 사용자인터페이스(UI)간의 정보 교환을 위한 규칙이나 알고리즘 의미
+ */
+
+import com.podoseee.model.dao.MenuDAO;
+import com.podoseee.model.dto.CategoryDTO;
+import com.podoseee.model.dto.MenuDTO;
+
+import java.sql.Connection;
+
+import static com.podoseee.common.JDBCTemplate.*;
+
+public class MenuService {
+
+    // 신규 카테고리와 메뉴 동시 추가
+    public int registCategoryAndMenu(CategoryDTO category, MenuDTO menu){
+        int result = 0; // 현재 비즈니스 로직의 최종 결과 담을 변수 (모든 작업이 성공되면 1, 아니면 0)
+
+        MenuDAO dao = new MenuDAO();
+
+        Connection conn = getConnection();
+
+        int result1 = dao.insertCategory(conn, category);
+        int result2 = dao.insertMenu(conn, menu);
+
+        if(result1 > 0 && result2 > 0){
+            commit(conn);
+            result = 1;
+        }else{
+            rollback(conn);
+        }
+
+        close(conn);
+
+        return result;
+    }
+
+    public int registCategoryAndMenu2(CategoryDTO category,MenuDTO menu){
+        // 신규 카테고리 등록 후 등록시 생성된 카테고리번호로 메뉴 등록
+
+        int result = 0; // 해당 작업들 전체의 성공여부를 판변할 면수 (최종결과)
+        MenuDAO menuDao = new MenuDAO();
+
+        Connection conn = getConnection();
+
+        // 1) 신규 카테고리 등록
+        int result1 = menuDao.insertCategory(conn, category);
+        // 2) 1번과정으로 등록된 카테고리번호를 조회
+        int currCategoryCode = menuDao.selectCurrentCategoryCode(conn);
+        menu.setCategoryCode(currCategoryCode);
+        // 3) 신규 메뉴 등록
+        int result2 = menuDao.insertMenu(conn, menu);
+
+        if(result1 > 0 && result2 > 0){
+            commit(conn);
+            result = 1;
+        }else {
+            rollback(conn);
+        }
+
+        return result;
+
+    }
+
+
+}
